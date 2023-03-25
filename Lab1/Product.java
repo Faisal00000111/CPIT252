b18e754915a944a8cf18d5d99dc770b758ce8550
Faisal00000111 <faiselalzahrani010@gmail.com>
2023-03-26 01:25:51 +0300

    Activity7

--- Lab1/Product.java
+++ Lab1/Product.java
@@ -5,13 +5,25 @@ public abstract class Product {
     private double price;
     private String name;
     private static int quantity;
+    public double weight;
 
-    public Product(int id, double price, String name){
+    public Product(int id, double price, String name, double weight){
       this.id = id;
       this.price = price;
       this.name = name;
       Product.quantity ++;
+      this.weight = weight;
     }
+
+    public double getWeight() {
+      return weight;
+    }
+
+    public void setWeight(double weight) {
+      // setter to add additional logic to control how weight is changed.
+      this.weight = weight;
+    }
+
 
     public void applySaleDiscount(double percentage){
       this.price = this.price - ((percentage/100) * this.price);
