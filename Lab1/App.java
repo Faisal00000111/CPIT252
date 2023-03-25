fea1a8aade28e28ff0e4508d6e1f3927a5f59789
Faisal00000111 <faiselalzahrani010@gmail.com>
2023-03-26 00:42:00 +0300

    Change Product class to Abstract

--- Lab1/App.java
+++ Lab1/App.java
@@ -1,10 +1,14 @@
 package Lab1;
-  
-  public class App {
-      public static void main(String[]args){
-          Product p1 = new Product(6745, 5.50, "Penne Pasta");
-          Product p2 = new Product(8853, 6.50, "Spaghetti Pasta");
-          Product p3 = new Product(2106, 4.50, "Linguine Pasta");
-          System.out.println("Total Quantity: " + p3.getTotalQuantity());
-      }
+
+import java.time.LocalDate;
+
+public class App{
+
+  public static void main(String[]args){
+    FoodProduct p = new FoodProduct(1234, 9.99, "water", LocalDate.parse("2022-06-07"));
+    FoodProduct p4 = new FoodProduct(3452, 10.0, "Cheddar Cheese", 
+    LocalDate.parse("2022-06-07"));
+    ElectricProduct p5 = new ElectricProduct(4875, 30.0, "Extension cord", "220v");
+    
   }
+}
